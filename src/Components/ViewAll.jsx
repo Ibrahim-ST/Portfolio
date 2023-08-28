8a73573393a6a6c92df5123bead49e38fbbe3651
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-08-28 23:46:57 +0600

    updated button

--- src/Components/ViewAll.jsx
+++ src/Components/ViewAll.jsx
@@ -19,8 +19,7 @@ const ViewAll = ({ title, aosData }) => {
               <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
               <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
                 <span className="relative text-white uppercase">{title}</span>
-              </span>
-             
+              </span>             
           </Link>
         </p>
       </section>
